3a43fea031c8632e38af7e58196ee16b29f3da01
lucas <lucas.glasner@ceaza.cl>
2023-08-09 17:05:45 -0400

    reescribiendo load functions

--- load.py
+++ load.py
@@ -9,44 +9,31 @@
 # ---------------------------------------------------------------------------- #
 # ---------------------------------- Imports --------------------------------- #
 # ---------------------------------------------------------------------------- #
-
 import os
 import datetime
 from glob import iglob,glob
+
 import xarray as xr
 import numpy as np
 import pandas as pd
 
-from numerics import regrid
 from params import *
 
-# ---------------------------------------------------------------------------- #
-# ---------------------------- Load data functions --------------------------- #
-# ---------------------------------------------------------------------------- #
 
-
-
-
-# ------------------------------ FORECAST TOOLS ------------------------------ #
-def last_forecast_path(which):
+# ------------------------------ PATH FUNCTIONS ------------------------------ #
+def raise_incorrect_type(which):
     """
-    Return last aviable forecast of each type
+    Raise value error if incorrect forecast type
 
     Args:
         which (str): kind of forecast (atm, ocean ,wave)
-
-    Returns:
-        str: path to netcdf file
     """
     if which not in ['atm','ocean','wave']:
         raise ValueError('Which parameter must indicate only some\
-                          of these ["ocean","wave", "atm"]')  
-    path = os.popen("ls "+eval(which+'_forecast_dir')+"/*.nc | sort | tail -n 1").read()
-    path = path.replace('\n','')
+                          of these ["ocean","wave", "atm"]') 
+    return
 
-    return path
-
-def first_forecast_date(which):
+def first_forecast_path(which):
     """
     Return the first aviable forecast of each type
 
@@ -56,378 +43,82 @@ def first_forecast_date(which):
     Returns:
         str: path to netcdf file
     """
-    if which not in ['atm','ocean','wave']:
-        raise ValueError('Which parameter must indicate only some\
-                          of these ["ocean","wave", "atm"]')  
-    path = os.popen("ls "+eval(which+'_forecast_dir')+"/*.nc | sort | head -n 1").read()
-    path = path.replace('\n','')
-    data = load_forecast_data(path,which)
-    time = pd.to_datetime(data.time.item())
-    return time
+    path = glob(eval(f'{which}_globalparams')['forecast_dir']+'/*.nc')
+    path = sorted(path)[0]
+    return path
 
-def target_date(forecast_directory, validationdata_directory):
+def first_forecast_date(which):
     """
-    Find out which is the last aviable date between
-    forecast and validation data
+    Return the date of the first aviable forecast of each type
+
+    Args:
+        which (str): kind of forecast (atm, ocean ,wave)
+
     Returns:
-        pandas Timestamp: last aviable data
+        str: first forecast date
     """
-    last_forecast = iglob(forecast_directory)
-    last_forecast = max(list(map(os.path.getctime,last_forecast)))
-    last_forecast = datetime.datetime.fromtimestamp(last_forecast)
+    path = first_forecast_path(which)
+    fdir     = eval(f'{which}_globalparams')['forecast_dir']
+    fsuffix  = eval(f'{which}_globalparams')['forecast_suffix']
+    date = path.replace(fdir,'')
+    date = date.replace(fsuffix,'')
+    date = date.replace('/','')
+    date = date.replace('.nc','')
+    date = pd.to_datetime(date).strftime('%F')
+    return date
+    
 
-    last_validation = iglob(validationdata_directory)
-    last_validation = max(list(map(os.path.getctime,last_validation)))
-    last_validation = datetime.datetime.fromtimestamp(last_validation)
+def last_forecast_path(which):
+    """
+    Return last aviable forecast of each type
+    Args:
+        which (str): kind of forecast (atm, ocean ,wave)
 
-    return min(last_forecast,last_validation).date()
+    Returns:
+        str: path to netcdf file
+    """
+    path = glob(eval(f'{which}_globalparams')['forecast_dir']+'/*.nc')
+    path = sorted(path)[-1]
+
+    return path
 
 
 def forecast_path(date, which):
     """
-    Return forecast path for a type and date. If asked for today
-    or for the future returns last aviable forecast.
-    If forecast exists but is corrupted, return last valid forecast.
-
+    Return forecast path for a type and specific date.
+    
     Args:
-        which (str): kind of forecast (atm, ocean ,wave)
+        which (str): type of forecast (atm, ocean ,wave)
         date  (str): %Y-%m-%d
 
     Returns:
         str: path to netcdf file
     """
-    #Some checks
-    if pd.to_datetime(date) < first_forecast_date(which):
-        fforecast = first_forecast_date(which).strftime('%F')
-        raise ValueError('Forecast are only aviable for dates after '+fforecast)
-    if which not in ['atm','ocean','wave']:
-        raise ValueError('Which parameter must indicate only some\
-                          of these ["ocean","wave", "atm"]') 
-    if pd.to_datetime(date).date() == datetime.datetime.now().date():
+    # Some checks
+    raise_incorrect_type(which)
+    
+    date      = pd.to_datetime(date)
+    firstdate = pd.to_datetime(first_forecast_date(which)) 
+    # If date goes before any aviable forecast raise error
+    if date<firstdate:
+        raise RuntimeError(f'Forecast are only aviable for dates after {firstdate}')
+    # If date is from today or the future return last valid forecast
+    if pd.to_datetime(date).date() >= datetime.datetime.now().date():
         path = last_forecast_path(which)
-    else:  
-        path = os.popen("ls "+eval(which+'_forecast_dir')+"/*.nc | grep "+date).read()
-        path = path.replace('\n','')
-        if path.replace(which+'_forecast_dir','') == '':
-            print('          '+which.upper()+' forecast for '+date+
-                  ' doesnt exist, trying with yesterday...')
-            ndate = pd.to_datetime(date)-pd.Timedelta(days=1)
-            ndate = ndate.strftime("%F")
-            return forecast_path(ndate,which)
-        path = path.replace('\n','') 
-    return path
-        
-
-def load_wrf(path, **kwargs):
-    """ 
-    load a wrf model output and adjust for the package to work
-    other arguments are passed to xarray.open_dataset() function
-    Args:
-        path (str): path to netcdf file
-
-    Returns:
-        XDataSet: xarray loaded dataset
-    """
-    data = xr.open_dataset(path, engine='netcdf4', **kwargs)
-    p = data.attrs['SIMULATION_START_DATE']
-    data = data.sortby('XTIME')
-    data = data.rename({'south_north':'y',
-                        'west_east':'x',
-                        'XTIME':'leadtime',
-                        'XLONG':'lon',
-                        'XLAT':'lat'})
-    data.coords['time'] = pd.to_datetime(p, format="%Y-%m-%d_%H:%M:%S")
-    
-    # Regrid to rectangular grid
-    lonmin,lonmax = data.lon.min().item(), data.lon.max().item()
-    latmin,latmax = data.lat.min().item(), data.lat.max().item()
-    dlon,dlat = data.lon.diff('x').min(),data.lat.diff('y').min()
-    dlon,dlat = abs(dlon.item()),abs(dlat.item())
-    hres      = min(dlon,dlat)
-    husk = xr.Dataset(coords={'lat':(['y'], np.arange(latmin,latmax+hres,hres)),
-                                'lon':(['x'], np.arange(lonmin,lonmax+hres,hres))})
-    data = regrid(data,husk).rename({'x':'lon','y':'lat'})
-    return data
-
-def load_mercator(path, **kwargs):
-    """ 
-    load a mercator model output and adjust for the package to work
-    other arguments are passed to xarray.open_dataset() function
-    Args:
-        path (str): path to netcdf file
-
-    Returns:
-        XDataSet: xarray loaded dataset
-    """
-    data = xr.open_dataset(path, engine='netcdf4', **kwargs)
-    data = data.squeeze()
-    data = data.rename({'latitude':'lat',
-                    'longitude':'lon',
-                    'time':'leadtime'})
-    p = path.split("/")[-1].split(".")[0]
-    data.coords['time'] = pd.to_datetime(p,format="%Y-%m-%d")
-
-    return data
-    
-def load_forecast_data(path, which,**kwargs):
-    """
-    Load netcdf forecast data
-
-    Args:
-        path (str): path to netcdf file
-        which (str): kind of forecast (ocean, atm, wave) 
-
-    Raises:
-        ValueError: If wrong kind of forecast is selected
-
-    Returns:
-        XDataSet: xarray with the loaded forecast data
-    """
-    if which=='ocean':
-        return load_mercator(path, **kwargs)
-    elif which=='wave':
-        return load_mercator(path, **kwargs)
-    elif which=='atm':
-        return load_wrf(path, **kwargs)
     else:
-        raise ValueError('Which parameter must indicate only some\
-            of these ["ocean","wave", "atm"]')
+        fdir     = eval(f'{which}_globalparams')['forecast_dir']
+        fsuffix  = eval(f'{which}_globalparams')['forecast_suffix']
+        fdformat = eval(f'{which}_globalparams')['forecast_dateformat']
+        path     = '{}/{}'.format(fdir,fsuffix)
+        path     = path+'{}.nc'.format(date.strftime(fdformat))
     
-def init_wrf(path):
-    """
-    Requieres netcdf library installed on path.
-    From the metadata grabs the wrf initialization date.
-    
-    Args:
-        path (str): path to a wrf output netcdf
-
-    Returns:
-        str: forecast initialization date
-    """
-    command = "ncdump -i -v XTIME "+path+" | grep XTIME"
-    init = os.popen(command+" | tail -n 1 | awk '{ print $3 }'").read()
-    init = init.replace(",","").replace("\"","")
-    init = pd.to_datetime(init).strftime('%F %HUTC')
-    return init
-    
-def init_mercator(path, which):
-    """
-    Requieres netcdf library installed on path.
-    From the metadata grabs the wrf initialization date.
-    
-    Args:
-        path (str): path to a mercator output netcdf
-
-    Returns:
-        str: forecast initialization date
-    """
-    if which=='ocean':
-        command = "ncdump -i -v time "+path+" | grep time"
-        init = os.popen(command+" | tail -n 1 | awk '{ print $3 }'").read()
-        init = init.replace(",","").replace("\"","")
-        init = pd.to_datetime(init).strftime('%F %HUTC')
-    elif which=='wave':
-        command = "ncdump -i -v time "+path+" | grep time"
-        init = os.popen(command+" | tail -n 1 | awk '{ print $3 }'").read()
-        init = init.replace(",","").replace("\"","")
-        init = pd.to_datetime(init).strftime('%F %HUTC')
-    else:
-        raise ValueError('Which parameter must indicate only some\
-            of these ["ocean","wave"]')   
-    return init
-
-def load_forecast_ini(path, which):
-    """
-    Load netcdf forecast data
-
-    Args:
-        path (str): path to netcdf file
-        which (str): kind of forecast (ocean, atm, wave) 
-
-    Raises:
-        ValueError: If wrong kind of forecast is selected
-
-    Returns:
-        str: initialization date of the forecast type requested
-    """
-    if which=='ocean':
-        return init_mercator(path,'ocean')
-    elif which=='wave':
-        return init_mercator(path, 'wave')
-    elif which=='atm':
-        return init_wrf(path)
-    else:
-        raise ValueError('Which parameter must indicate only some\
-            of these ["ocean","wave", "atm"]')
-
-
-# ------------------- Diagnostic and validation data tools ------------------- #
-
-
-def load_altimeters(idate, centerhour=datetime.datetime.now(), hwidth=4):
-    """
-    Function for loading as a table the wave/wind altimeter data from 
-    copernicus database.
-
-    Args:
-        idate (str): Target date to recover data
-        centerhour (datetime, optional): _description_. Defaults to datetime.datetime.now().
-        hwidth (int, optional): _description_. Defaults to 4.
-
-    Returns:
-        _type_: _description_
-    """
-    centerhour = datetime.datetime.now()
-    print('Loading altimeter data...')
-    pddate           = pd.to_datetime(idate)
-    altimeters_paths = sorted(glob(wave_altimeter_dir+'/*'))
-    altimeters_data  = []
-    names            = []
-    for a in altimeters_paths:
-        try:
-            p = a+'/'+str(pddate.year)+'/'+ str('%02d' % pddate.month)+'/*'+idate.replace("-","")+'*'
-            data = xr.open_mfdataset(p).load().drop_duplicates('time').sortby('time').to_dataframe()
-            data['longitude'] = (data['longitude']+180)%360-180
-            times = slice(pddate-pd.Timedelta(hours=hwidth)+pd.Timedelta(hours=centerhour.hour),
-                          pddate+pd.Timedelta(hours=hwidth)+pd.Timedelta(hours=centerhour.hour))
-            data = data.loc[times]
-            # data = data.loc[idate]
-            altimeters_data.append(data)
-            print('          '+a.split("/")[-1],': Done')
-            names.append(a.split("/")[-1].split("_")[-2].split("-")[0])
-            del data
-        except Exception as e:
-            print('          '+a.split("/")[-1],':', e)
-
-    altimeters_data = pd.concat(altimeters_data, keys=names)
-    altimeters_data = altimeters_data[['latitude','longitude','VAVH','WIND_SPEED']]
-    altimeters_data.columns = ['lat','lon',waveheight_name, windspeed_name]
-    return altimeters_data.drop_duplicates()
-         
-
-def load_ascat(idate, **kwargs):
-    """
-    Function for loading ASCAT L4 data as xarray
-
-    Args:
-        idate (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    p = pd.to_datetime(idate)
-    ascat_path = glob('data/ASCAT/'+p.strftime('%Y%m%d')+'00*')
-
-
-    if ascat_path!=[]:
-        ascat_path = ascat_path[0]
-        data = xr.open_dataset(ascat_path, **kwargs).squeeze()
-        data = data.rename({'longitude':'lon',
-                            'latitude':'lat'})
-        data = data.sortby('lat').sortby('lon')
-        data = data[['eastward_wind','northward_wind']]
-        data = data.rename({'eastward_wind':uwnd_name,'northward_wind':vwnd_name})
-        data[windspeed_name] = np.hypot(data[uwnd_name],data[vwnd_name])
-        return data
-    else:
-        print('         ASCAT data for '+idate+' doesnt exists!')
-        return
-    
-
-def load_ccmp(idate, **kwargs):
-    """
-    Function for loading CCMP Wind data as xarray
-
-    Args:
-        idate (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    ccmp_path = 'data/CCMPWINDS/'
-    file_name = 'CCMP_RT_Wind_Analysis_'+idate.replace('-','')+'_V02.1_L3.0_RSS.nc'
-    ccmp_path = ccmp_path+file_name
-    print('          '+ccmp_path)
-    data = xr.open_dataset(ccmp_path, **kwargs).squeeze()
-    data = data.rename({'latitude':'lat', 'longitude':'lon',
-                        'uwnd':uwnd_name,'vwnd':vwnd_name})
-    data['lon'] = (data['lon']+180)%360-180
-    data = data.sortby('lon').sortby('lat')
-    data[windspeed_name] = np.hypot(data[uwnd_name], data[vwnd_name])
-    data = data.sel(lat=slice(diagnostics_mapsextent[2]-1,diagnostics_mapsextent[3]+1),
-                    lon=slice(diagnostics_mapsextent[0]-1,diagnostics_mapsextent[1]+1))
-    return data
-
-def load_ostia(idate, **kwargs):
-    """
-    Function for loading OSTIA SST L4 satellite data as xarray
-
-    Args:
-        idate (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    p = pd.to_datetime(idate)
-    ostia_path = 'data/OSTIA/'+p.strftime('%Y%m%d')+\
-        '-UKMO-L4HRfnd-GLOB-v01-fv02-OSTIA.nc'
-    if os.path.isfile(ostia_path):
-        data = xr.open_dataset(ostia_path, **kwargs).analysed_sst-273.15
-        data = data.resample({'time':'d'}).mean().squeeze()
-        data = data.sel(lat=slice(diagnostics_mapsextent[2]-1,diagnostics_mapsextent[3]+1),
-                    lon=slice(diagnostics_mapsextent[0]-1,diagnostics_mapsextent[1]+1))
-        return data
-    else:
-        print('         OSTIA data for '+idate.strftime('%F')+' doesnt exists!')
-        return
-
-def load_tpxo(idate, path=ocean_tidemodel_file, n_constituents=10, **kwargs):
-    """
-    This functions load the TPXO ocean tides dataset and computes the 
-    amplitude and phase of each tidal constituent.
-
-    Args: 
-        idate (str): forecast date
-        path (str): ocean tide model file (netcdf)
-        n_constituents (int): number of tidal constituents (1..10)
-        **kwargs are passed to xarray.open_dataset()
-    Returns:
-        _type_: _description_
-    """
-#   print('Loading tide model data: '+ocean_tidemodel_file)
-    if os.path.isfile(ocean_tidemodel_file):
-        tides = xr.open_dataset(path, **kwargs)
-        tides = tides.isel(periods=slice(0,n_constituents))
-    else:
-        # print('Tide model: '+ocean_tidemodel_file+'doesnt exists!')
-        return
-    # print('Tidal constituents: '+tides.components)
-    try:
-        tides = tides.rename({'lat_r':'lat','lon_r':'lon'})
-        tides.coords['lon'] = (tides.coords['lon'] + 180) % 360 - 180
-    except Exception as e:
-        print(e)
+    # Check if constructed path exist else raise error
+    if os.path.isfile(path):
+        # All good
         pass
-    name=np.array(tides.components.split(' ')).squeeze()[:n_constituents]
-    tides = tides.assign_coords({'name':('periods',name)})
-    tides = tides.swap_dims({'periods':'name'})
-    # print('Computing amplitude and phase for each constituent...')
-    tides['tssh_complex']   = tides.ssh_r+tides.ssh_i*np.sqrt(-1, dtype=complex)
-    tides['tssh_amplitude'] = np.abs(tides['tssh_complex'])
+    else:
+        raise RuntimeError(f'{path} doesnt exists!')
+    
+    return path
 
-    tides['tssh_phase']     = np.mod(np.rad2deg(xr.apply_ufunc(np.angle,tides['tssh_complex'])),360)
-    tides['t_time']         = pd.to_datetime(idate)
-    
-    # if nodal_correct:
-    #     # print('Performing nodal corrections for '+idate+'...')
-    #     pf,pu,t0,phase_mkB  = egbert_correct(modified_julian_day(pd.to_datetime(idate).date()),
-    #                                          0,0,0)
-    #     pf = pf.loc[name]
-    #     pu = pu.loc[name]
-        
-    #     tides['tssh_amplitude'] = tides['tssh_amplitude']*pf.to_xarray().rename({'index':'name'})
-    #     tides['tssh_phase']     = np.mod(-tides['tssh_phase']-pu.to_xarray().rename({'index':'name'}),360)
-        
-    # print('Done')
-    return tides
-    
+# ------------------------------ LOAD FUNCTIONS ------------------------------ #
--- params.py
+++ params.py
@@ -10,104 +10,60 @@ import datetime
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 
-# ---------------------------------------------------------------------------- #
-#                                 GENERAL STUFF                                #
-# ---------------------------------------------------------------------------- #
+
+# ------------------------------- GENERAL STUFF ------------------------------ #
 FORECAST_DATE        = datetime.datetime.now().strftime('%F')
-EXECUTION_DIRECTORY  = '/home/lucas/projects/CEAZAMAR_KITCHEN/CEAZAMAR_FORECAST'
+EXECUTION_DIRECTORY  = '/home/lucas/CEAZAMAR_FORECAST'
 NDAYS_REGIONAL = 10
 NHOURS_LOCAL   = 24*9-12
 N_JOBS = 10
 
-#LEADTIME MAPS GENERAL CONFIGURATION
+# -------------------- LEADTIME MAPS GENERAL CONFIGURATION ------------------- #
 landpolygon_path       = 'data/regiones.gpkg'
 atm_mapsextent         = [-75,-70.8,-34,-28]
 ocean_mapsextent       = [-75,-70.8,-34,-28]
 wave_mapsextent        = [-75,-70.8,-34,-28]
-diagnostics_mapsextent = [-75,-70.8,-34,-28]
 
-MAPS_GRID    = (2,5)
-MAPS_FIGSIZE = (15,8)
+# ----------------------------- ATMOSPHERIC MODEL ---------------------------- #
+atm_globalparams = {
+    'forecast_model':'WRF-Ceaza',
+    'forecast_dir':'/home/lucas/storage/FORECAST/WRF/WRFMARd02',
+    'forecast_suffix':'wrfCeazaOp_d02_',
+    'forecast_dateformat':'%FT00Z',
+    'horizontal_resolution':'4km',
+    }
 
-MAPS_LOCS    = {'Valparaíso':(-33.046,-71.613),
-                'Los Vilos':(-31.904,-71.499),
-                'Huentelauquen':(-31.621,-71.568),
-                'Talcaruca':(-30.476,-71.697),
-                'La Serena':(-29.878,-71.286),
-                'Chañaral de\nAceituno':(-29.064,-71.514)}
+atm_variables = {
+    'uwnd':'U10',
+    'vwnd':'V10',
+    'lsm':'LANDMASK',
+    'precip':'RAIN',
+    'rh2':'RH2',
+    't2m':'T2',
+    'lowclouds':'LOWCLOUDS',
+    'midclouds':'MIDCLOUDS',
+    'highclouds':'HIGHCLOUDS',    
+}
 
-# ---------------------------------------------------------------------------- #
-#                                  ATMOSPHERE                                  #
-# ---------------------------------------------------------------------------- #
-atm_model_name      = 'WRF-Ceaza'
-atm_validation_name = 'ASCAT'
-# ----------------------------------- paths ---------------------------------- #
-atm_forecast_dir    = 'data/FORECAST/WRF/U10V10'           
-atm_validation_dir  = 'data/ASCAT'                       
-# --------------------------- atmospheric variables -------------------------- #
-uwnd_name           = 'U10'
-vwnd_name           = 'V10'
-lsm_name            = 'LANDMASK'
-windspeed_name      = 'WS'
-winddir_name        = 'WDIR'
-
-# ---------------------------------------------------------------------------- #
-#                                     WAVES                                    #
-# ---------------------------------------------------------------------------- #
-wave_model_name = 'MFWAM-Meteofrance'
-# ----------------------------------- paths ---------------------------------- #
-wave_forecast_dir  = 'data/FORECAST/MERCATOR/WAVES'
-wave_hindcast_dir  = 'data/FORECAST/MERCATOR/WAVES/HINDCAST'
-wave_altimeter_dir = str('data/WAVES_SATELLITE_NRT/nrt.cmems-du.eu/Core/'+
-                     'WAVE_GLO_PHY_SWH_L3_NRT_014_001')
-# ------------------------------ wave variables ------------------------------ #
-waveheight_name   = 'VHM0'
-swell1height_name = 'VHM0_SW1'
-swell2height_name = 'VHM0_SW2'
-
-waveperiod_name   = 'VTPK'
-swell1period_name = 'VTM01_SW1'
-swell2period_name = 'VTM01_SW2'
-
-wavedir_name      = 'VPED'
-swell1dir_name    = 'VMDR_SW1'
-swell2dir_name    = 'VMDR_SW2'
-# ---------------------------------------------------------------------------- #
-#                                     OCEAN                                    #
-# ---------------------------------------------------------------------------- #
-ocean_model_name       = 'NEMO-Meteofrance'
-ocean_validation_name  = 'OSTIA'
-# ----------------------------------- paths ---------------------------------- #
-ocean_tidemodel_file   = 'data/TPXO7.nc'
-ocean_forecast_dir     = 'data/FORECAST/MERCATOR/PHYSICS'
-ocean_hindcast_dir     = 'data/FORECAST/MERCATOR/PHYSICS/HINDCAST'
-ocean_climatology_file = 'data/CLIMATOLOGIES.nc'
-ocean_validation_dir   = 'data/OSTIA'
-# ------------------------------ variable names ------------------------------ #
-sst_name  = 'thetao'
-uo_name   = 'uo'
-vo_name   = 'vo'
-ssh_name  = 'zos'
+# -------------------------------- OCEAN MODEL ------------------------------- #
+ocean_globalparams = {
+    'forecast_model':'MERCATOR-NEMO',
+    'forecast_dir':'/home/lucas/storage/FORECAST/MERCATOR/PHYSICS',
+    'forecast_suffix':'',
+    'forecast_dateformat':'%F',
+    'horizontal_resolution':'9km',
+}
 
 
-# ----------------------------- CUSTOM COLORMAPS ----------------------------- #
-hexcolors = ["#4cb164","#5cb35b","#6cb452","#7cb649","#8cb840","#9cb937",
-             "#adbb2d","#bdbc24","#cdbe1b","#ddc012","#edc109","#fdc300",
-             "#fab302","#f7a204","#f49206","#f18208","#ee720a","#ea610c",
-             "#e7510e","#e44110","#e13112","#de2014","#db1016"]
-colors    = [mcolors.to_rgb(c) for c in hexcolors]
-plt.cm.register_cmap(name='traffic_ceaza',
-                     cmap=mcolors.ListedColormap(colors))
-del hexcolors, colors
+# -------------------------------- WAVE MODEL -------------------------------- #
+wave_globalparams = {
+    'forecast_model':'MERCATOR-MFWAM',
+    'forecast_dir':'/home/lucas/storage/FORECAST/MERCATOR/WAVES',
+    'forecast_suffix':'',
+    'forecast_dateformat':'%F',
+    'horizontal_resolution':'9km',
+}
 
-hexcolors = ["#1c8dbe","#3197c4","#45a2ca","#5aacd0","#6fb6d6","#83c1dc",
-             "#98cbe1","#acd6e7","#c1e0ed","#d6eaf3","#eaf5f9","#ffffff",
-             "#fce9ea","#f8d4d5","#f5bebf","#f2a8aa","#ef9295","#eb7d80",
-             "#e8676b","#e55156","#e23b40","#de262b","#db1016"]
-colors    = [mcolors.to_rgb(c) for c in hexcolors]
-plt.cm.register_cmap(name='redblue_ceaza',
-                     cmap=mcolors.ListedColormap(colors))
-del hexcolors, colors
 
 
 
